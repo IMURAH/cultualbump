20045f6569ff96d92a1b0b829a506a0b89cd7c28
kzv0015 <keganvanginkel@gmail.com>
2021-11-21 22:11:44 -0600

    Added language changing by switching homecountry

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -12,12 +12,14 @@ export class DataService {
   continents: Continent[];
   homeCountry: Country;
   categories: Category[];
+  client: HttpClient;
 
   constructor(http: HttpClient, private storage: Storage) { 
     this.continents = [];
     this.categories = [];
+    this.client = http;
 
-    http.get('assets/bumps.json', {})
+    this.client.get('assets/bumps.json', {})
       .subscribe(data => {
         // Split all countries into continents
         let continents = [];
@@ -94,6 +96,57 @@ export class DataService {
   setHomeCountry(home: Country) {
     this.homeCountry = home;
     this.storage.set('home_country', JSON.stringify(this.homeCountry));
+    let fileName = 'assets/bumps.json';
+    if (this.homeCountry.name == 'China') {
+      fileName = 'assets/bumps_CN.json';
+    }
+
+    this.client.get(fileName, {})
+        .subscribe(data => {
+          // Split all countries into continents
+          let continents = [];
+          let countries = [];
+          data['continent'].forEach(cont => {
+            continents.push(new Continent(cont.continent_name, cont.continent_id));
+          });
+          data['country'].forEach(count => {
+            let cont =  new Country(count.country_name, count.country_id, count.latitude, count.longitude, count.resourceName)
+            continents.find(function(c) {
+              return count.continent_id == c.id;
+            }).addCountry(cont);
+            countries.push(cont);
+          });
+          this.continents = continents;
+
+          // Split all behaviors into categories
+          let categories = [];
+          data['category'].forEach(cat => {
+            categories.push(new Category(cat.category_id, cat.category_name));
+          });
+          data['behavior'].forEach(behav => {
+            categories.find(function(c) {
+              return behav.category_id == c.id;
+            }).addBehavior(new Behavior(behav.behavior_id, behav.behavior_description));
+          });
+          this.categories = categories;
+
+          // Take all characteristics and put into appropriate countries
+          let characteristics = [];
+          data['characteristic'].forEach(char => {
+            characteristics.push(new Characteristic(char.characteristics_id, char.behavior_id, char.characteristics_description, null))
+          });
+
+          data['nation_characteristic'].forEach(nat => {
+            let count = countries.find(function(c) {
+              return c.id == parseInt(nat.country_id);
+            });
+            let char = characteristics.find(function(c) {
+              return c.id == parseInt(nat.characteristics_id);
+            });
+            count.addCharacteristic(char.createCopy(nat.extra_description, count.resourse));
+          });
+          // console.log(continents)
+        });
   }
 
   getHomeCountry(): Country {
